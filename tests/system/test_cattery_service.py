9c3abe7cdfd16973d7ab9826f947714a7b0a17b5
Michael Tom-Wing <mtomwing@demonware.net>
2015-09-14 18:59:55 -0700

    System tests can now startup a CatteryService

--- tests/system/test_cattery_service.py
+++ tests/system/test_cattery_service.py
@@ -1,37 +1,15 @@
 import pytest
-import requests
 
-
-class HttpBaseClient(object):
-
-    def __init__(self, base_url):
-        self._base_url = base_url
-
-    def get(self, *args, **kwargs):
-        return requests.get("{base_url}/{url_parts}".format(
-            base_url=self._base_url, url_parts="/".join(args)
-        ), **kwargs)
-
-    def post(self, *args, **kwargs):
-        return requests.post("{base_url}/{url_parts}".format(
-            base_url=self._base_url, url_parts="/".join(args)
-        ), **kwargs)
-
-    def delete(self, *args, **kwargs):
-        return requests.delete("{base_url}/{url_parts}".format(
-            base_url=self._base_url, url_parts="/".join(args)
-        ), **kwargs)
-
-
-
+from util import HttpBaseClient, start_service
 
 
 @pytest.yield_fixture
 def cattery_client():
     # start the service
+    service = start_service('catinabox.services.cattery')
     yield HttpBaseClient("http://localhost:8000")
+    service.terminate()
     # stop the service
-
 
 
 def test__get_all_cats__no_cats__returns_no_cats(cattery_client):
