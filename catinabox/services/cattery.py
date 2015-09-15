7512846eaa3b0e1e17cbed666d0cbd7dad66c55c
christie <christie@demonware.net>
2015-09-14 19:06:10 -0700

    System test that adds a cat

--- catinabox/services/cattery.py
+++ catinabox/services/cattery.py
@@ -1,35 +1,21 @@
+import json
 from nameko.web.handlers import http
-
-
-# Source: http://user.xmission.com/~emailbox/ascii_cats.htm
-CAT_SITTING_ON_FENCE = '''
-             *     ,MMM8&&&.            *
-                  MMMM88&&&&&    .
-                 MMMM88&&&&&&&
-     *           MMM88&&&&&&&&
-                 MMM88&&&&&&&&
-                 'MMM88&&&&&&'
-                   'MMM8&&&'      *
-          |\___/|
-          )     (             .              '
-         =\     /=
-           )===(       *
-          /     \
-          |     |
-         /       \
-         \       /
-  _/\_/\_/\__  _/_/\_/\_/\_/\_/\_/\_/\_/\_/\_
-  |  |  |  |( (  |  |  |  |  |  |  |  |  |  |
-  |  |  |  | ) ) |  |  |  |  |  |  |  |  |  |
-  |  |  |  |(_(  |  |  |  |  |  |  |  |  |  |
-  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |
-  jgs|  |  |  |  |  |  |  |  |  |  |  |  |  |
-'''
 
 
 class CatteryService(object):
     name = 'cattery'
+    _cats = []
 
-    @http('GET', '/')
-    def index(self, request):
-        return 200, CAT_SITTING_ON_FENCE
+    ############################################################################
+    # cats
+    ############################################################################
+
+    @http('GET', '/cats')
+    def get_cats(self, request):
+        return 200, json.dumps(self._cats)
+
+    @http('POST', '/cats')
+    def create_cat(self, request):
+        cat_info = json.loads(request.get_data())
+        self._cats.append(cat_info)
+        return 200
--- tests/system/test_cattery_service.py
+++ tests/system/test_cattery_service.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 
 from util import HttpBaseClient, start_service
@@ -12,7 +13,16 @@ def cattery_client():
     # stop the service
 
 
-def test__get_all_cats__no_cats__returns_no_cats(cattery_client):
-    result = cattery_client.get("cats")
-    assert result.json() == []
-    assert result.status_code == 200
+class TestGetCats(object):
+
+    def test__get_cats__no_cats__returns_no_cats(self, cattery_client):
+        result = cattery_client.get('cats')
+        assert result.json() == []
+        assert result.status_code == 200
+
+    def test__get_cats__cat_added__returns_the_cats(self, cattery_client):
+        cattery_client.post('cats', data=json.dumps({"name": "Theodora"}))
+        result = cattery_client.get('cats')
+
+        assert result.json() == [{"name": "Theodora"}]
+        assert result.status_code == 200
