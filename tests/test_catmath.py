7172eda6103bf19d4188392124880f9d8c8320a4
ransomv <rvarley2001@yahoo.com>
2015-10-11 14:11:23 -0700

    Added test for cat math

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,13 +1,16 @@
-# from catinabox import catmath
+from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(5) == 25
+    # assert True
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(.5) == 2.5
+    # assert True
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
+    # assert True
