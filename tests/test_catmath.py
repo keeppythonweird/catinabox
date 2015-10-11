4528329f63da3c0435aae9b8049d67256ca7c226
marijane white <mwhite@thetus.com>
2015-10-11 14:13:06 -0700

    catmath tests!

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,13 +1,18 @@
-# from catinabox import catmath
+from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    cat_age = 8
+    human_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert human_age == 40
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    cat_age = 0.8
+    human_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert human_age == 4
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    human_age = catmath.cat_years_to_hooman_years(0)
+    assert human_age == 0
