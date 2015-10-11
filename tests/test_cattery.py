8c0bc30712539aea1cdc0899fc70a9e363b3eb25
Scott Molloy <scott.l.molloy@gmail.com>
2015-10-11 14:49:47 -0700

    Use test fixtures on cattery

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -3,37 +3,38 @@ import pytest
 from catinabox import cattery
 
 
+@pytest.fixture()
+def cats():
+    return cattery.Cattery()
+
 ###########################################################################
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Snookums"])
-    assert c.cats == ["Fluffy", "Snookums"]
-    assert c.num_cats == 2
 
+def test__add_cats__succeeds(cats):
+    cats.add_cats(["Fluffy", "Snookums"])
+    assert cats.cats == ["Fluffy", "Snookums"]
+    assert cats.num_cats == 2
 
 ###########################################################################
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Junior"])
-    c.remove_cat("Fluffy")
-    assert c.cats == ["Junior"]
-    assert c.num_cats == 1
+
+def test__remove_cat__succeeds(cats):
+    cats.add_cats(["Fluffy", "Junior"])
+    cats.remove_cat("Fluffy")
+    assert cats.cats == ["Junior"]
+    assert cats.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(cats):
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Fluffles")
+        cats.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy"])
+def test__remove_cat__cat_not_in_cattery__fails(cats):
+    cats.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Snookums")
+        cats.remove_cat("Snookums")
