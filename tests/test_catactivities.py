9cb28354443c8c984512694090ebe9dc2f2e5ea1
christie <christie@demonware.net>
2015-10-11 11:53:32 -0700

    Add all of the solutions

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,14 +1,17 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    catactivities.cat_nap(500)
+    mock_sleep.assert_called_with(500)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    with pytest.raises(catactivities.NapWillNotBeSatisfying):
+        catactivities.cat_nap(5)
+    assert mock_sleep.call_count == 0
