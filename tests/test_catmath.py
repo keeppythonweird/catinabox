6087872375e8edf6084a3688ff46cff6ffd58aeb
Jlaventall <j.laventall@gmail.com>
2015-10-11 14:35:48 -0700

    added test for safecatmatch.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -8,9 +8,9 @@ def test__cat_years_to_hooman_years__middle_age__succeeds():
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    cat_age = 0.5
+    cat_age = 0.1
     hooman_age = catmath.cat_years_to_hooman_years(cat_age)
-    assert hooman_age == 0.25
+    assert hooman_age == 0.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
