37da83f5996d72581d311bd493858f8e775ba6dc
medullaskyline <kelly.e.iverson@gmail.com>
2015-10-11 14:45:58 -0700

    cattery tests done

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -6,34 +6,34 @@ from catinabox import cattery
 ###########################################################################
 # add_cats
 ###########################################################################
+@pytest.fixture
+def a_cattery():
+    return cattery.Cattery()
 
-def test__add_cats__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Snookums"])
-    assert c.cats == ["Fluffy", "Snookums"]
-    assert c.num_cats == 2
+
+def test__add_cats__succeeds(a_cattery):
+    a_cattery.add_cats(["Fluffy", "Snookums"])
+    assert a_cattery.cats == ["Fluffy", "Snookums"]
+    assert a_cattery.num_cats == 2
 
 
 ###########################################################################
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Junior"])
-    c.remove_cat("Fluffy")
-    assert c.cats == ["Junior"]
-    assert c.num_cats == 1
+def test__remove_cat__succeeds(a_cattery):
+    a_cattery.add_cats(["Fluffy", "Junior"])
+    a_cattery.remove_cat("Fluffy")
+    assert a_cattery.cats == ["Junior"]
+    assert a_cattery.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(a_cattery):
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Fluffles")
+        a_cattery.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy"])
+def test__remove_cat__cat_not_in_cattery__fails(a_cattery):
+    a_cattery.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Snookums")
+        a_cattery.remove_cat("Snookums")
