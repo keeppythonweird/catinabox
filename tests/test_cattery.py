2ee26d5d136e884cd78978bb93c0225f51204fc3
atifar <tyatya68@gmail.com>
2016-01-09 12:12:22 -0800

    Parametrize test_cattery to also cover McCattery.

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -1,11 +1,14 @@
 import pytest
 
-from catinabox import cattery
+from catinabox import cattery, mccattery
 
 
-@pytest.fixture
-def my_awesome_fixture():
-    return cattery.Cattery()
+@pytest.fixture(params=[
+    cattery.Cattery,
+    mccattery.McCattery,
+])
+def my_awesome_fixture(request):
+    return request.param()
 
 
 ###########################################################################
@@ -16,6 +19,8 @@ def test__add_cats__succeeds(my_awesome_fixture):
     my_awesome_fixture.add_cats(["Fluffy", "Snookums"])
     assert my_awesome_fixture.cats == ["Fluffy", "Snookums"]
     assert my_awesome_fixture.num_cats == 2
+    if isinstance(my_awesome_fixture, mccattery.McCattery):
+        assert len(my_awesome_fixture.history) == 2
 
 
 ###########################################################################
@@ -27,11 +32,15 @@ def test__remove_cat__succeeds(my_awesome_fixture):
     my_awesome_fixture.remove_cat("Fluffy")
     assert my_awesome_fixture.cats == ["Junior"]
     assert my_awesome_fixture.num_cats == 1
+    if isinstance(my_awesome_fixture, mccattery.McCattery):
+        assert len(my_awesome_fixture.history) == 3
 
 
 def test__remove_cat__no_cats__fails(my_awesome_fixture):
     with pytest.raises(cattery.CatNotFound):
         my_awesome_fixture.remove_cat("Fluffles")
+    if isinstance(my_awesome_fixture, mccattery.McCattery):
+        assert len(my_awesome_fixture.history) == 0
 
 
 def test__remove_cat__cat_not_in_cattery__fails(my_awesome_fixture):
