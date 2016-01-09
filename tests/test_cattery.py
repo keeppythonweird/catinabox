2a80b09f33a513d4eeaa1747bf9cdb5a0e22df22
atifar <tyatya68@gmail.com>
2016-01-09 00:32:48 -0800

    Refactor cattery tests using a fixture.

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -3,37 +3,38 @@ import pytest
 from catinabox import cattery
 
 
+@pytest.fixture
+def my_awesome_fixture():
+    return cattery.Cattery()
+
+
 ###########################################################################
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Snookums"])
-    assert c.cats == ["Fluffy", "Snookums"]
-    assert c.num_cats == 2
+def test__add_cats__succeeds(my_awesome_fixture):
+    my_awesome_fixture.add_cats(["Fluffy", "Snookums"])
+    assert my_awesome_fixture.cats == ["Fluffy", "Snookums"]
+    assert my_awesome_fixture.num_cats == 2
 
 
 ###########################################################################
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Junior"])
-    c.remove_cat("Fluffy")
-    assert c.cats == ["Junior"]
-    assert c.num_cats == 1
+def test__remove_cat__succeeds(my_awesome_fixture):
+    my_awesome_fixture.add_cats(["Fluffy", "Junior"])
+    my_awesome_fixture.remove_cat("Fluffy")
+    assert my_awesome_fixture.cats == ["Junior"]
+    assert my_awesome_fixture.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(my_awesome_fixture):
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Fluffles")
+        my_awesome_fixture.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy"])
+def test__remove_cat__cat_not_in_cattery__fails(my_awesome_fixture):
+    my_awesome_fixture.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Snookums")
+        my_awesome_fixture.remove_cat("Snookums")
