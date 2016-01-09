edd8c2dfd23b9b7a4c9c0ec09c5c898ce2a3fa0f
atifar <tyatya68@gmail.com>
2016-01-09 10:16:11 -0800

    Enhance catmath tests by parametrizing them.

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,12 +1,24 @@
+import pytest
 from catinabox import catmath
 
 
-def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert catmath.cat_years_to_hooman_years(8.5) == 42.5
+@pytest.mark.parametrize('age, expected', [
+    (7, 35),
+    (8.7, 43.5),
+    (11.0, 55.0),
+])
+def test__cat_years_to_hooman_years__middle_age__succeeds(age, expected):
+    assert catmath.cat_years_to_hooman_years(age) == expected
 
 
-def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert catmath.cat_years_to_hooman_years(0.3) == 1.5
+@pytest.mark.parametrize('age, expected', [
+    (0.7, 3.5),
+    (0.02, 0.1),
+])
+def test__cat_years_to_hooman_years__less_than_one_year__succeeds(
+    age, expected
+):
+    assert catmath.cat_years_to_hooman_years(age) == expected
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
