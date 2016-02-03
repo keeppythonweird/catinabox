956667fad80b72bbb898ed753b2890307a9bec8c
James Young <jyoung@OpenRoad-Blue.local>
2016-02-02 21:17:35 -0800

    Another commit from James

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -3,11 +3,16 @@ import pytest
 from catinabox import cattery
 
 
+@pytest.fixture(params=[cattery.Cattery])
+def c(request):
+    return request.param()
+
+
 ###########################################################################
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
+def test__add_cats__succeeds(c):
     c = cattery.Cattery()
     c.add_cats(["Fluffy", "Snookums"])
     assert c.cats == ["Fluffy", "Snookums"]
@@ -18,7 +23,7 @@ def test__add_cats__succeeds():
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
+def test__remove_cat__succeeds(c):
     c = cattery.Cattery()
     c.add_cats(["Fluffy", "Junior"])
     c.remove_cat("Fluffy")
@@ -26,13 +31,13 @@ def test__remove_cat__succeeds():
     assert c.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
+def test__remove_cat__no_cats__fails(c):
     c = cattery.Cattery()
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
+def test__remove_cat__cat_not_in_cattery__fails(c):
     c = cattery.Cattery()
     c.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
