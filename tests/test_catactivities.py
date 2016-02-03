cbe0de4d8b899b3d47fa58e5719ff0ea7fe06496
PYARIA <jadetong@gmail.com>
2016-02-02 21:19:20 -0800

    catactivities test cases

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,16 +1,17 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    cat_nap_length = 1000
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    catactivities.cat_nap(cat_nap_length)
+    mock_sleep.assert_called_with(cat_nap_length)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    # cat_nap_length = 30
-    # cat_nap_status = catactivities.cat_nap(5)
-    assert True
+    cat_nap_length = 30
+    with pytest.raises(catactivities.NapWillNotBeSatisfying):
+        catactivities.cat_nap(cat_nap_length)
