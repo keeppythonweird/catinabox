2d94532e316e9ad563b3b7506d47cfd78ca7f689
Meghan Halton <meghalton@gmail.com>
2016-02-02 20:55:21 -0800

    Step_5: Add fixtures to cattery tests
    Add a fixture to remove initialisation of the cattery in every test.
    
    Signed-off-by: Meghan Halton <meghalton@gmail.com>

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -4,11 +4,19 @@ from catinabox import cattery
 
 
 ###########################################################################
+# fixtures
+###########################################################################
+
+@pytest.fixture
+def c():
+    return cattery.Cattery()
+
+
+###########################################################################
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
-    c = cattery.Cattery()
+def test__add_cats__succeeds(c):
     c.add_cats(["Fluffy", "Snookums"])
     assert c.cats == ["Fluffy", "Snookums"]
     assert c.num_cats == 2
@@ -18,7 +26,7 @@ def test__add_cats__succeeds():
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
+def test__remove_cat__succeeds(c):
     c = cattery.Cattery()
     c.add_cats(["Fluffy", "Junior"])
     c.remove_cat("Fluffy")
@@ -26,14 +34,12 @@ def test__remove_cat__succeeds():
     assert c.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(c):
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
+def test__remove_cat__cat_not_in_cattery__fails(c):
     c.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Snookums")
