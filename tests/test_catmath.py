b143c9369ddc49c75a89e28748683c9ca69a427f
Connor Mayer <cmayer@demonware.net>
2016-02-02 20:05:46 -0800

    Improved test coverage like crazy.

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,13 +1,17 @@
-# from catinabox import catmath
+from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    cat_age = 8
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == cat_age * catmath.NUM_HOOMAN_YEARS_IN_CAT_YEAR
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    cat_age = 0.1
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == cat_age * catmath.NUM_HOOMAN_YEARS_IN_CAT_YEAR
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
