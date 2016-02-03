62e29e90fede1b1f9fbae455aee3dad1f7d2a2c5
Michelle La Haye <michelle.lahaye@gmail.com>
2016-02-02 20:21:03 -0800

    add catmath test for middle age

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,7 +2,10 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    if catmath.cat_years_to_hooman_years(7) == 35:
+        assert True
+    else:
+        assert False
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
