1ea406e15de9fd5468fbe985972480e4a742c7fd
Michelle La Haye <michelle.lahaye@gmail.com>
2016-02-02 20:14:05 -0800

    adding tests for catmath cases 0 and less than 1 year

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,4 +1,4 @@
-# from catinabox import catmath
+from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
@@ -6,8 +6,14 @@ def test__cat_years_to_hooman_years__middle_age__succeeds():
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    if catmath.cat_years_to_hooman_years(0.1) < 1:
+        assert True
+    else:
+        assert False
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    if catmath.cat_years_to_hooman_years(0) == 0:
+        assert True
+    else:
+        assert False
