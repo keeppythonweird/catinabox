fe5a2ec02d0b76c0cea45df11b53a828963fcdad
Meghan Halton <meghalton@gmail.com>
2016-02-02 21:08:42 -0800

    Step_6: Mock out time in catactivities tests
    Mock out time in catactivities tests so that they don't take years to
    run.
    
    Signed-off-by: Meghan Halton <meghalton@gmail.com>

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,14 +1,16 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_time = mocker.patch.object(time, "sleep", autospec=True)
+    catactivities.cat_nap(5*60)
+    mock_time.assert_called_once_with(5*60)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mocker.patch.object(time, "sleep", autospec=True)
+    with pytest.raises(catactivities.NapWillNotBeSatisfying):
+        catactivities.cat_nap(5)
