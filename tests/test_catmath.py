eb26a9492cc70a5eba3d79ba0717d131c9a94d28
Serena Chan <ssc26@sfu.ca>
2016-02-02 20:08:28 -0800

    test 2

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -6,6 +6,7 @@ def test__cat_years_to_hooman_years__middle_age__succeeds():
     hooman_age = catmath.cat_years_to_hooman_years(cat_age)
     assert hooman_age == 25
 
+
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
     cat_age = 0.1
     hooman_age = catmath.cat_years_to_hooman_years(cat_age)
@@ -16,3 +17,5 @@ def test__cat_years_to_hooman_years__0__returns_0():
     cat_age = 0
     hooman_age = catmath.cat_years_to_hooman_years(cat_age)
     assert hooman_age == 0
+
+
