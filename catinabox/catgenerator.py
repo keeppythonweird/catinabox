66fe6fc6c700257240167cccee48931b93d64779
bobcatfish <bobcatfish@gmail.com>
2016-05-28 16:28:25 -0700

    Add solution for refactoring exercise
    This solution refactors `cat_generator` into a glue between
    two responsibilities: generating a name and generating a birthday.

--- catinabox/catgenerator.py
+++ catinabox/catgenerator.py
@@ -18,20 +18,25 @@ class CouldNotGetNameError(Exception):
         )
 
 
+def get_name():
+    try:
+        result = requests.get(NAME_GENERATOR_API_ENDPOINT)
+        return result.json()[0]
+    except requests.exceptions.RequestException as e:
+        raise CouldNotGetNameError(e)
+
+
+def get_birthday():
+    current_time = int(time.time())
+    birthday = random.randint(
+        current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
+        current_time)
+    birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
+                                      time.gmtime(birthday))
+    return birthday_datetime
+
+
 def cat_generator():
     while True:
-        try:
-            result = requests.get(NAME_GENERATOR_API_ENDPOINT)
-            name = result.json()[0]
-        except requests.exceptions.RequestException as e:
-            raise CouldNotGetNameError(e)
-
-        current_time = int(time.time())
-        birthday = random.randint(
-            current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
-            current_time)
-        birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
-                                          time.localtime(birthday))
-
-        yield {"name": name,
-               "birthday": birthday_datetime}
+        yield {"name": get_name(),
+               "birthday": get_birthday()}
