023c4f0ce3f602f68760df615572e9698c3e9e28
bobcatfish <bobcatfish@gmail.com>
2016-05-28 16:28:25 -0700

    Add solution for refactoring exercise
    This solution refactors `cat_generator` into a glue between
    two responsibilities: generating a name and generating a birthday.

--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -1,9 +1,37 @@
-# import pytest
+import pytest
+import requests
 
-# from catinabox import catgenerator
+from catinabox import catgenerator
 
 
-# Write tests for the refactored `catinabox.catgenerator`
+def test__get_name(mocker):
+    mocker.patch("requests.get").return_value.json.return_value = ["Francis"]
+    cat_name = catgenerator.get_name()
+    assert cat_name == "Francis"
 
-def test__():
-    pass
+
+def test__get_name__requests_error__raises(mocker):
+    mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
+    with pytest.raises(catgenerator.CouldNotGetNameError):
+        catgenerator.get_name()
+
+
+def test__get_birthday(mocker):
+    mocker.patch("time.time", return_value=catgenerator.SECONDS_IN_YEAR * 35)
+    randint = mocker.patch("random.randint",
+                           return_value=catgenerator.SECONDS_IN_YEAR * 2)
+    birthday = catgenerator.get_birthday()
+    assert birthday == "1972-01-01 00:00:00"
+    randint.assert_called_with(catgenerator.SECONDS_IN_YEAR * 5,
+                               catgenerator.SECONDS_IN_YEAR * 35)
+
+
+def test__cat_generator(mocker):
+    mocker.patch.object(catgenerator, "get_name", return_value="Moe")
+    mocker.patch.object(catgenerator, "get_birthday", return_value="birthday")
+    cat_generator = catgenerator.cat_generator()
+
+    assert next(cat_generator) == {"name": "Moe",
+                                   "birthday": "birthday"}
+    assert next(cat_generator) == {"name": "Moe",
+                                   "birthday": "birthday"}
