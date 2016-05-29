d49ab801bf7eb39fc8b62cda9285037cf73926ba
Tim Allard <tallard@squarespace.com>
2016-05-29 11:14:04 -0700

    adding params for raises

--- tests/test_safecatmath.py
+++ tests/test_safecatmath.py
@@ -29,22 +29,39 @@ def test__cat_years_to_hooman_years__0__returns_0(tc, expct):
     assert safecatmath.cat_years_to_hooman_years(tc) == expct
 
 
-def test__cat_years_to_hooman_years__less_0__raises():
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(-10)
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (-10, safecatmath.InvalidAge),
+    (-15, safecatmath.InvalidAge),
+    (-7, safecatmath.InvalidAge)
+])
+def test__cat_years_to_hooman_years__less_0__raises(tc, expct):
+    with pytest.raises(expct):
+        safecatmath.cat_years_to_hooman_years(tc)
 
 
-def test__cat_years_to_hooman_years__older_than_1000__raises():
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(2225.2)
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (2225.2, safecatmath.InvalidAge),
+    (50000, safecatmath.InvalidAge),
+    (1000.000001, safecatmath.InvalidAge)
+])
+def test__cat_years_to_hooman_years__older_than_1000__raises(tc, expct):
+    with pytest.raises(expct):
+        safecatmath.cat_years_to_hooman_years(tc)
 
 
-def test__cat_years_to_hooman_years__string__raises():
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years("105")
+@pytest.mark.parametrize(('tc', 'expct'), [
+    ("105", safecatmath.InvalidAge),
+    ("7", safecatmath.InvalidAge),
+    ("5555555", safecatmath.InvalidAge)
+])
+def test__cat_years_to_hooman_years__string__raises(tc, expct):
+    with pytest.raises(expct):
+        safecatmath.cat_years_to_hooman_years(tc)
 
 
-def test__cat_years_to_hooman_years__nan__raises():
-    hooman_age = float('nan')
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(hooman_age)
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (float('nan'), safecatmath.InvalidAge),
+])
+def test__cat_years_to_hooman_years__nan__raises(tc, expct):
+    with pytest.raises(expct):
+        safecatmath.cat_years_to_hooman_years(tc)
