e02358e4f2cad4ce893cc141dca1be14e6122eeb
Andrey Portnoy <aportnoy@ucsd.edu>
2016-05-29 11:45:19 -0700

    corrected fixtures

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -6,13 +6,12 @@ from catinabox import cattery
 ###########################################################################
 # add_cats
 ###########################################################################
-@pytest.fixture(scope='session')
-def cattery_client():
+@pytest.fixture(scope='function')
+def c():
     return cattery.Cattery()
 
 
-def test__add_cats__succeeds():
-    c = cattery.Cattery()
+def test__add_cats__succeeds(c):
     c.add_cats(["Fluffy", "Snookums"])
     assert c.cats == ["Fluffy", "Snookums"]
     assert c.num_cats == 2
@@ -22,22 +21,19 @@ def test__add_cats__succeeds():
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
-    c = cattery.Cattery()
+def test__remove_cat__succeeds(c):
     c.add_cats(["Fluffy", "Junior"])
     c.remove_cat("Fluffy")
     assert c.cats == ["Junior"]
     assert c.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(c):
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
+def test__remove_cat__cat_not_in_cattery__fails(c):
     c.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Snookums")
