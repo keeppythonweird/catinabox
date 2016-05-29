c7ce4390fd6dd4e15070e7f19dd491dab7bf11ae
Doug Campbell <dcampbell@gogoair.com>
2016-05-29 14:33:57 -0700

    refactored catgenerator and added unit tests

--- catinabox/catgenerator.py
+++ catinabox/catgenerator.py
@@ -18,20 +18,28 @@ class CouldNotGetNameError(Exception):
         )
 
 
+def get_cat_name(url=NAME_GENERATOR_API_ENDPOINT):
+    try:
+        result = requests.get(url)
+        name = result.json()[0]
+    except requests.exceptions.RequestException as e:
+        raise CouldNotGetNameError(e)
+    return name
+
+
+def get_birthday():
+    current_time = int(time.time())
+    birthday = random.randint(
+        current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
+        current_time)
+    birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
+                                      time.localtime(birthday))
+    return birthday_datetime
+
+
 def cat_generator():
     while True:
-        try:
-            result = requests.get(NAME_GENERATOR_API_ENDPOINT)
-            name = result.json()[0]
-        except requests.exceptions.RequestException as e:
-            raise CouldNotGetNameError(e)
-
-        current_time = int(time.time())
-        birthday = random.randint(
-            current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
-            current_time)
-        birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
-                                          time.localtime(birthday))
-
+        name = get_cat_name()
+        birthday = get_birthday()
         yield {"name": name,
-               "birthday": birthday_datetime}
+               "birthday": birthday}
