efb80addca37e1a204254c85ada2e46da9ad0072
Doug Campbell <dcampbell@gogoair.com>
2016-05-29 11:56:22 -0700

    used mock to make unit tests for catactivities

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,14 +1,18 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep.return_value = None
+    catactivities.cat_nap(301)
     assert True
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep.return_value = None
+    with(pytest.raises(catactivities.NapWillNotBeSatisfying)):
+        catactivities.cat_nap(299)
