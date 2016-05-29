1d97dc7a29bd7c71e4a644b12b644a164b884a49
John Mulder <john_c_mulder@yahoo.com>
2016-05-29 09:32:07 -0700

    added some actual testing to the tests for the catmath module.

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,24 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(6) == 30
+    assert catmath.cat_years_to_hooman_years(12) == 60
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.2) == 1
+    assert catmath.cat_years_to_hooman_years(0.5) == 2.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
+    assert catmath.cat_years_to_hooman_years(0.0) == 0.0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+    assert catmath.is_cat_leap_year(2017) is False
+    assert catmath.is_cat_leap_year(2000) is True
+    assert catmath.is_cat_leap_year(2100) is False
