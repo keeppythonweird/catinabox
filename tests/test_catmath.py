eca2cdd409d735d29c3ddde790bbd8af63bdc13c
Tim Allard <tallard@squarespace.com>
2016-05-29 09:39:00 -0700

    add decorator - parameterize

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,19 +1,33 @@
 from catinabox import catmath
+import pytest
 
 
-def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (6, 30),
+    (5, 25),
+    (4, 20),
+    (10, 50)
+])
+def test__cat_years_to_hooman_years__middle_age__succeeds(tc, expct):
+    assert catmath.cat_years_to_hooman_years(tc) == expct
 
 
-def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (.5, .5 * 5),
+    (.75, .75 * 5),
+    (.45, .45 * 5)
+])
+def test__cat_years_to_hooman_years__less_than_one_year__succeeds(tc, expct):
+    assert catmath.cat_years_to_hooman_years(tc) == expct
 
 
-def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+@pytest.mark.parametrize(('tc', 'expct'), [
+    (0, 0),
+])
+def test__cat_years_to_hooman_years__0__returns_0(tc, expct):
+    assert catmath.cat_years_to_hooman_years(tc) == expct
 
 
 # BONUS MATERIAL FOR STEP 2
-
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
