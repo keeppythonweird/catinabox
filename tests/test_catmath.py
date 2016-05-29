70a0446ff253f952f27aef387c1ab75c35d6b55e
Kojo Idrissa <kojo.idrissa@gmail.com>
2016-05-29 10:45:16 -0700

    added tests

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,16 +1,19 @@
 from catinabox import catmath
+cth = catmath.NUM_HOOMAN_YEARS_IN_CAT_YEAR
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    for i in range(7, 10):
+        assert catmath.cat_years_to_hooman_years(i) == i * cth
+    # assert catmath.cat_years_to_hooman_years(8) == 40
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.5) == 2.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
