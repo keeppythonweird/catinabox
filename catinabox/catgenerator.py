b77a5ff6391d8827a0e768b473e1d2b2dc57140d
martpat <martpat@email.cz>
2016-06-08 23:57:38 +0200

    Added test coverage over catgenerator.py and test_catgenerator.py.

--- catinabox/catgenerator.py
+++ catinabox/catgenerator.py
@@ -2,7 +2,6 @@ import random
 import requests
 import time
 
-
 NAME_GENERATOR_API_ENDPOINT = "http://namey.muffinlabs.com/name.json"
 # ignoring leap years for now
 SECONDS_IN_YEAR = 365 * 24 * 60 * 60
@@ -18,20 +17,28 @@ class CouldNotGetNameError(Exception):
         )
 
 
+def get_name():
+    try:
+        result = requests.get(NAME_GENERATOR_API_ENDPOINT)
+        name = result.json()[0]
+    except requests.exceptions.RequestException as e:
+        raise CouldNotGetNameError(e)
+    return name
+
+
+def get_birthday_datetime():
+    current_time = int(time.time())
+    birthday = random.randint(
+        current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
+        current_time)
+    return time.strftime('%Y-%m-%d %H:%M:%S',
+                         time.localtime(birthday))
+
+
 def cat_generator():
     while True:
-        try:
-            result = requests.get(NAME_GENERATOR_API_ENDPOINT)
-            name = result.json()[0]
-        except requests.exceptions.RequestException as e:
-            raise CouldNotGetNameError(e)
-
-        current_time = int(time.time())
-        birthday = random.randint(
-            current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
-            current_time)
-        birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
-                                          time.localtime(birthday))
+        name = get_name()
+        birthday_datetime = get_birthday_datetime()
 
         yield {"name": name,
                "birthday": birthday_datetime}
--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -1,9 +1,35 @@
-# import pytest
+import pytest
+import requests
 
-# from catinabox import catgenerator
+from catinabox import catgenerator
 
 
 # Write tests for the refactored `catinabox.catgenerator`
 
-def test__():
-    pass
+
+def test__get_name(mocker):
+    mocker.patch('requests.get').return_value.json.return_value = ['Bob']
+    cat_name = catgenerator.get_name()
+    assert cat_name == 'Bob'
+
+
+def test__get_name_raises(mocker):
+    mocker.patch('requests.get', side_effect=requests.exceptions.Timeout)
+    with pytest.raises(catgenerator.CouldNotGetNameError):
+        catgenerator.get_name()
+
+
+def test__get_birthday_datetime(mocker):
+    mocker.patch('time.time').return_value = catgenerator.SECONDS_IN_YEAR * 10
+    mocker.patch('random.randint').return_value = \
+        catgenerator.SECONDS_IN_YEAR * 10
+    assert catgenerator.get_birthday_datetime() == '1979-12-30 01:00:00'
+
+
+def test__cat_generator(mocker):
+    mocker.patch.object(catgenerator, 'get_name').return_value = 'Alice'
+    mocker.patch.object(catgenerator, 'get_birthday_datetime'). \
+        return_value = '1979-12-30 01:00:00'
+    cat = catgenerator.cat_generator()
+    assert next(cat) == {"name": 'Alice', "birthday": '1979-12-30 01:00:00'}
+    assert next(cat) == {"name": 'Alice', "birthday": '1979-12-30 01:00:00'}
