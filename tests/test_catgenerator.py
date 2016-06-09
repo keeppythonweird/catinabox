b542f50f3d181d3ad19e1b4c0e25e64ade1b7d49
martpat <martpat@email.cz>
2016-06-09 12:58:43 +0200

    Modified catgenerator tests -> added autospec. Added tests for catactivites -> coverage at 100%.

--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -8,28 +8,32 @@ from catinabox import catgenerator
 
 
 def test__get_name(mocker):
-    mocker.patch('requests.get').return_value.json.return_value = ['Bob']
+    mocker.patch('requests.get', autospec=True). \
+        return_value.json.return_value = ['Bob']
     cat_name = catgenerator.get_name()
     assert cat_name == 'Bob'
 
 
 def test__get_name_raises(mocker):
-    mocker.patch('requests.get', side_effect=requests.exceptions.Timeout)
+    mocker.patch('requests.get', side_effect=requests.exceptions.Timeout,
+                 autospec=True)
     with pytest.raises(catgenerator.CouldNotGetNameError):
         catgenerator.get_name()
 
 
 def test__get_birthday_datetime(mocker):
-    mocker.patch('time.time').return_value = catgenerator.SECONDS_IN_YEAR * 20
-    mocker.patch('random.randint').return_value = \
+    mocker.patch('time.time', autospec=True).return_value = \
+        catgenerator.SECONDS_IN_YEAR * 20
+    mocker.patch('random.randint', autospec=True).return_value = \
         catgenerator.SECONDS_IN_YEAR * 1
     assert catgenerator.get_birthday_datetime() == '1971-01-01 00:00:00'
 
 
 def test__cat_generator(mocker):
-    mocker.patch.object(catgenerator, 'get_name').return_value = 'Alice'
-    mocker.patch.object(catgenerator, 'get_birthday_datetime'). \
-        return_value = '1979-12-30 01:00:00'
+    mocker.patch.object(catgenerator, 'get_name',
+                        autospec=True).return_value = 'Alice'
+    mocker.patch.object(catgenerator, 'get_birthday_datetime',
+                        autospec=True).return_value = '1979-12-30 01:00:00'
     cat = catgenerator.cat_generator()
     assert next(cat) == {"name": 'Alice', "birthday": '1979-12-30 01:00:00'}
     assert next(cat) == {"name": 'Alice', "birthday": '1979-12-30 01:00:00'}
