3acb913981185e727f268a999f7af22423b0979a
GalRavil <gal.ravil93@gmail.com>
2016-10-30 11:40:29 +0300

    Tests for cattery.py

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -3,11 +3,16 @@ import pytest
 from catinabox import cattery
 
 
+@pytest.fixture()
+def cattery_clien():
+    return cattery.Cattery()
+
+
 ###########################################################################
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
+def test__add_cats__succeeds(cattery_clien):
     c = cattery.Cattery()
     c.add_cats(["Fluffy", "Snookums"])
     assert c.cats == ["Fluffy", "Snookums"]
@@ -18,7 +23,7 @@ def test__add_cats__succeeds():
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
+def test__remove_cat__succeeds(cattery_clien):
     c = cattery.Cattery()
     c.add_cats(["Fluffy", "Junior"])
     c.remove_cat("Fluffy")
@@ -26,13 +31,13 @@ def test__remove_cat__succeeds():
     assert c.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
+def test__remove_cat__no_cats__fails(cattery_clien):
     c = cattery.Cattery()
     with pytest.raises(cattery.CatNotFound):
         c.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
+def test__remove_cat__cat_not_in_cattery__fails(cattery_clien):
     c = cattery.Cattery()
     c.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
