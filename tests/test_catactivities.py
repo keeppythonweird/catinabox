fbbe88cd85d1547545da023e30b521e59e865766
deathcod <chinmay.rakshit@gmail.com>
2016-11-28 10:15:04 +0530

    new update

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,14 +1,20 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep = 1000
+    try:
+        catactivities.cat_nap(mock_sleep)
+    except Exception:
+        pytest.fail(catactivities.NapWillNotBeSatisfying)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep = 1
+    with pytest.raises(catactivities.NapWillNotBeSatisfying):
+        catactivities.cat_nap(mock_sleep)
