135bd4f3ff8276a5171282fc0901e6c9aeca9a91
Mark Jennings <mjennings@demonware.net>
2017-01-09 10:20:51 -0800

    Add unit tests for catmath.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,26 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(10) == 50
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.1) == 0.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+
+
+def test__is_cat_leap_year__fails():
+    assert catmath.is_cat_leap_year(2015) is False
+
+
+def test__is_cat_leap_year__2000():
+    assert catmath.is_cat_leap_year(2000) is True
