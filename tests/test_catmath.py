35d64759d324ea6e01448d328ac9b410b9c7a44b
hichkantana <samito975@gmail.com>
2017-01-21 13:29:44 -0800

    fix PEP-8 flag

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -12,6 +12,7 @@ def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
 def test__cat_years_to_hooman_years__0__returns_0():
     assert catmath.cat_years_to_hooman_years(0) == 0
 
+
 # what is this test doing?
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
