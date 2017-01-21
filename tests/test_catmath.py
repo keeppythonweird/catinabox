5793405d278ccdcd5923b48fc1fda7cfd48e793a
hichkantana <samito975@gmail.com>
2017-01-21 12:47:31 -0800

    fill the test cases body

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,16 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(5) == 25
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.2) == 1.0
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
-
-# BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
