9beb93d2a403e4830ec23013aa1088d643b23653
Mark Prykhoda <m.prykhoda@gmail.com>
2017-02-05 22:38:01 +0200

    first commit from 3 step about 2 step

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,15 +2,15 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(10) == 50
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.4) == 2.0
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
