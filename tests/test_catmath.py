2d622a19d2514022af9a6e0b82d2e63f2ea0434a
c0ffee0wl <c0ffee0wl@gmx.net>
2017-04-14 14:58:55 +0200

    Parameterize tests and fixtures

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,31 +1,23 @@
+import pytest
 from catinabox import catmath
 
 
-def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert catmath.cat_years_to_hooman_years(3) == 15
-
-
-def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert catmath.cat_years_to_hooman_years(0.2) == 1.0
-
-
-def test__cat_years_to_hooman_years__0__returns_0():
-    assert catmath.cat_years_to_hooman_years(0) == 0
+@pytest.mark.parametrize("input, expected", [
+    (3, 15),
+    (0.2, 1.0),
+    (0, 0)
+])
+def test__cat_years_to_hooman_years__succeeds(input, expected):
+    assert catmath.cat_years_to_hooman_years(input) == expected
 
 
 # BONUS MATERIAL FOR STEP 2
 
-def test__is_cat_leap_year__not_divisible_by_4__isnt_leap_year():
-    assert catmath.is_cat_leap_year(1757) is False
-
-
-def test__is_cat_leap_year__divisible_by_100__isnt_leap_year():
-    assert catmath.is_cat_leap_year(1900) is False
-
-
-def test__is_cat_leap_year__centurial_leap_year__is_leap_year():
-    assert catmath.is_cat_leap_year(2000) is True
-
-
-def test__is_cat_leap_year__typical_leap_year__is_leap_year():
-    assert catmath.is_cat_leap_year(2004) is True
+@pytest.mark.parametrize("input, expected", [
+    (1757, False),
+    (1900, False),
+    (2000, True),
+    (2004, True)
+])
+def test__is_cat_leap_year(input, expected):
+    assert catmath.is_cat_leap_year(input) is expected
--- tests/test_safecatmath.py
+++ tests/test_safecatmath.py
@@ -3,37 +3,19 @@ import pytest
 from catinabox import safecatmath
 
 
-def test__cat_years_to_hooman_years__middle_age__succeeds():
-    hooman_age = safecatmath.cat_years_to_hooman_years(7)
-    assert hooman_age == 35
+@pytest.mark.parametrize("input, expected", [
+    (7, 35),
+    (0.1, 0.5),
+    (0, 0)
+])
+def test__cat_years_to_hooman_years__succeeds(input, expected):
+    hooman_age = safecatmath.cat_years_to_hooman_years(input)
+    assert hooman_age == expected
 
 
-def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    hooman_age = safecatmath.cat_years_to_hooman_years(0.1)
-    assert hooman_age == 0.5
-
-
-def test__cat_years_to_hooman_years__0__returns_0():
-    hooman_age = safecatmath.cat_years_to_hooman_years(0)
-    assert hooman_age == 0
-
-
-def test__cat_years_to_hooman_years__less_0__raises():
+@pytest.mark.parametrize("input", [
+    -1, 10001, "5", float('nan')
+])
+def test__cat_years_to_hooman_years__raises(input):
     with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(-1)
-
-
-def test__cat_years_to_hooman_years__older_than_1000__raises():
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(1001)
-
-
-def test__cat_years_to_hooman_years__string__raises():
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years("5")
-
-
-def test__cat_years_to_hooman_years__nan__raises():
-    cat_age = float('nan')
-    with pytest.raises(safecatmath.InvalidAge):
-        safecatmath.cat_years_to_hooman_years(cat_age)
+        safecatmath.cat_years_to_hooman_years(input)
