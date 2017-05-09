8c733d7bbb660abc374aaef1d5a08a2a5adc07db
enigmaeth <enigmaeth@gmail.com>
2017-05-10 00:47:54 +0530

    added tests for safecatmath.py

--- tests/test_safecatmath.py
+++ tests/test_safecatmath.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 
 from catinabox import safecatmath
 
@@ -19,17 +19,21 @@ def test__cat_years_to_hooman_years__0__returns_0():
 
 
 def test__cat_years_to_hooman_years__less_0__raises():
-    assert True
+    with pytest.raises(Exception):
+        assert safecatmath.cat_years_to_hooman_years(-0.1)
 
 
 def test__cat_years_to_hooman_years__older_than_1000__raises():
-    assert True
+    with pytest.raises(Exception):
+        assert safecatmath.cat_years_to_hooman_years(7855)
 
 
 def test__cat_years_to_hooman_years__string__raises():
-    assert True
+    with pytest.raises(Exception):
+        assert safecatmath.cat_years_to_hooman_years("noon")
 
 
 def test__cat_years_to_hooman_years__nan__raises():
-    # hooman_age = float('nan')
-    assert True
+    with pytest.raises(Exception):
+        hooman_age = float('nan')
+        assert safecatmath.cat_years_to_hooman_years(hooman_age)
