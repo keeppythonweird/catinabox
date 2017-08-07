aa926ee219913ed782b4636bdb9a867dabf10456
U-GLRSLAWNY\gonzala <gonzala@win7gonzala1.GLRSLAWNY.com>
2017-08-07 14:33:50 -0400

    added my first tests to test_catmath.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,34 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(6) >= 30
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(.5) < 5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+
+
+def test__is_cat_leap_year__fails():
+    assert catmath.is_cat_leap_year(1986) is False
+
+
+def test__is_cat_leap_year_div_by_4():
+    assert catmath.is_cat_leap_year(15) is False
+
+
+def test__is_cat_leap_year_div_4_100():
+    assert catmath.is_cat_leap_year(1984) is True
+
+
+def test__is_cat_leap_year_div_4_100_400():
+    assert catmath.is_cat_leap_year(2000) is True
