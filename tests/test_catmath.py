8ce687d4882382fb2846359dbcb3ae007fba11d1
Thegaijin <semaganda@outlook.com>
2017-10-29 01:53:38 +0300

    Refactoring tests in test_cathmath.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,19 +1,19 @@
-from catinabox import catmath
+from catinabox.catmath import cat_years_to_hooman_years, is_cat_leap_year
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert cat_years_to_hooman_years(10) == 50
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert cat_years_to_hooman_years(0.3) == 1.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
-    assert catmath.is_cat_leap_year(2016) is True
+    assert is_cat_leap_year(2016) is True
