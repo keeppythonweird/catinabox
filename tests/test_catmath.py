60f85e3d876f3ce6e437ad5c779f8c820d43706b
Thegaijin <semaganda@outlook.com>
2017-10-29 02:28:22 +0300

    Fixed function name errors in test_catmath.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -14,7 +14,7 @@ def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-     catage = 0
+    catage = 0
     hooman_age = cat_years_to_hooman_years(catage)
     assert hooman_age == 0
 
@@ -24,12 +24,14 @@ def test__cat_years_to_hooman_years__0__returns_0():
 def test__is_cat_leap_year__is_leap_year():
     assert is_cat_leap_year(2016) is True
 
-def test__is_cat_leap_year__not_divisible by_4_is_not_leap_year():
+
+def test__is_cat_leap_year__not_divisible_by_4_is_not_leap_year():
     assert is_cat_leap_year(1999) is False
 
-def test__is_cat_leap_year__divisible by_100_is_not_leap_year():
+
+def test__is_cat_leap_year__divisible_by_100_is_not_leap_year():
     assert is_cat_leap_year(1900) is False
+
 
 def test__is_cat_leap_year__centurial_leap_year__is_leap_year():
     assert is_cat_leap_year(2000) is True
-
