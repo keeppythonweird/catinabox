02992113fe1f19298a0a0c1ebb16560983b1ba6e
Eric Thanenthiran <AirT@Erics-MacBook-Air.local>
2017-11-10 16:53:30 +0000

    TAke 2

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -13,17 +13,19 @@ def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
 def test__cat_years_to_hooman_years__0__returns_0():
     assert catmath.cat_years_to_hooman_years(0) == 0
 
-
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
 
+
 def test__is_cat_leap_century__succeeds():
-	assert catmath.is_cat_leap_year(1804) is True
+    assert catmath.is_cat_leap_year(1804) is True
+
 
 def test__is_cat_leap_quad_century__succeeds():
-	assert catmath.is_cat_leap_year(1600) is True
+    assert catmath.is_cat_leap_year(1600) is True
+
 
 def test__is_cat_leap_year__fails():
-	assert catmath.is_cat_leap_year(2017) is False
+    assert catmath.is_cat_leap_year(2017) is False
