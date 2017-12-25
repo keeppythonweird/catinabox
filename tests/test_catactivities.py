2bbdd582f6843df01d3118ae89e08cd7518d4ac3
Andrew Fodiman <fodiman@gmail.com>
2017-12-25 11:16:58 -0500

    add mocking and assertion of mocked time

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,14 +1,17 @@
-# import pytest
-# import time
+import pytest
+import time
 
-# from catinabox import catactivities
+from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    catactivities.cat_nap(catactivities.LENGTH_OF_SATISFYING_NAP+1)
+    mock_sleep.assert_called_with(catactivities.LENGTH_OF_SATISFYING_NAP+1)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    # mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
-    assert True
+    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    with pytest.raises(catactivities.NapWillNotBeSatisfying):
+        catactivities.cat_nap(catactivities.LENGTH_OF_SATISFYING_NAP-1)
+    mock_sleep.assert_not_called()
