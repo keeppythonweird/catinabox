5e72f2261ba1413d0d2aca69a0a2ab6789fa64eb
TadB <tadeusz.brzytwa@gmail.com>
2018-05-12 08:31:42 +0200

    pierwsze testy

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -3,14 +3,23 @@ from catinabox import catmath
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
     assert True
+    cat_year = 6
+    hooman_years = catmath.cat_years_to_hooman_years(cat_year)
+    assert hooman_years == 30
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
     assert True
+    cat_year = 0.3
+    hooman_years = catmath.cat_years_to_hooman_years(cat_year)
+    assert hooman_years == 1.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
     assert True
+    cat_year = 0
+    hooman_years = catmath.cat_years_to_hooman_years(cat_year)
+    assert hooman_years == 0
 
 
 # BONUS MATERIAL FOR STEP 2
