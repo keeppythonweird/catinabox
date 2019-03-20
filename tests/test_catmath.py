461b44bc715ca18602d4d1ac4e565b3130fc56be
Enthalpy Change <enthalpychange@users.noreply.github.com>
2019-03-19 21:04:47 -0700

    wrote some tests

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,18 +2,30 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(10) == 50
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0.5) == 2.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    assert catmath.cat_years_to_hooman_years(0) == 0
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
     assert catmath.is_cat_leap_year(2016) is True
+
+
+def test__is_cat_leap_year__not_leap_year__fails():
+    assert catmath.is_cat_leap_year(2015) is False
+
+
+def test__is_cat_leap_year__not_divisible_by_400__fails():
+    assert catmath.is_cat_leap_year(2100) is False
+
+
+def test__is_cat_leap_year__divisible_by_100__fails():
+    assert catmath.is_cat_leap_year(2000) is True
