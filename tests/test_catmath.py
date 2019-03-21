0d2adab018b95d8cdb068937deaa669ab4ceba0d
Havernis <11685796+Havernis@users.noreply.github.com>
2019-03-21 23:25:55 +0200

    add tests to safecatmath.py and fix tests in catmath to have the same format

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,15 +2,18 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert catmath.cat_years_to_hooman_years(10) == 50
+    expected_hooman_years = 50
+    assert catmath.cat_years_to_hooman_years(10) == expected_hooman_years
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert catmath.cat_years_to_hooman_years(0.1) == 0.5
+    expected_hooman_years = 0.5
+    assert catmath.cat_years_to_hooman_years(0.1) == expected_hooman_years
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert catmath.cat_years_to_hooman_years(0) == 0
+    expected_hooman_years = 0
+    assert catmath.cat_years_to_hooman_years(0) == expected_hooman_years
 
 
 # BONUS MATERIAL FOR STEP 2
