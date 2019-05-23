000b52cb7fd00d39ece5ab07fdf8dc7240abc1a4
Melanie Freshness <melanie.freshness@gmail.com>
2019-05-23 12:27:09 +0100

    initial commit

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,36 +2,28 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    cat_years = 7
-    hooman_years = catmath.cat_years_to_hooman_years(cat_years)
-    assert hooman_years == 35
+    expected = 50
+    result = catmath.cat_years_to_hooman_years(10)
+    assert expected == result
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    cat_years = 0.5
-    hooman_years = catmath.cat_years_to_hooman_years(cat_years)
-    assert hooman_years == 2.5
+    expected = 2.5
+    result = catmath.cat_years_to_hooman_years(0.5)
+    assert expected == result
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    cat_years = 0
-    hooman_years = catmath.cat_years_to_hooman_years(cat_years)
-    assert hooman_years == 0
+    expected = 0
+    result = catmath.cat_years_to_hooman_years(0)
+    assert expected == result
 
 
 # BONUS MATERIAL FOR STEP 2
 
 def test__is_cat_leap_year__succeeds():
+    assert catmath.is_cat_leap_year(3) is False
     assert catmath.is_cat_leap_year(2016) is True
-
-
-def test__is_cat_leap_year_2000__succeeds():
-    assert catmath.is_cat_leap_year(2000) is True
-
-
-def test__is_cat_leap_year_1900__fails():
-    assert catmath.is_cat_leap_year(1900) is False
-
-
-def test__is_cat_leap_year_2002__fails():
-    assert catmath.is_cat_leap_year(2002) is False
+    assert catmath.is_cat_leap_year(4) is True
+    assert catmath.is_cat_leap_year(100) is False
+    assert catmath.is_cat_leap_year(400) is True
