453ec0da15b489f32c0305eae1bc14c7e7ca9bf9
David Aponte <david.aponte@benevolent.ai>
2019-09-07 23:22:16 -0400

    added catgen tests

--- catinabox/catgenerator.py
+++ catinabox/catgenerator.py
@@ -1,4 +1,5 @@
-import random
+from numpy import random
+import numpy as np
 import requests
 import time
 
@@ -18,20 +19,39 @@ class CouldNotGetNameError(Exception):
         )
 
 
+def get_name(NAME_GENERATOR_API_ENDPOINT):
+    result = requests.get(NAME_GENERATOR_API_ENDPOINT)
+    return result.json()[0]
+
+
+def get_current_time():
+    return int(time.time())
+
+
+def get_birthday(current_time):
+    np.random.seed(123)
+    birthday = random.randint(
+        current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
+        current_time)
+    return birthday
+
+
+def to_datetime(birthday):
+    birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
+                                      time.localtime(birthday))
+    return birthday_datetime
+
+
 def cat_generator():
     while True:
         try:
-            result = requests.get(NAME_GENERATOR_API_ENDPOINT)
-            name = result.json()[0]
+            name = get_name(NAME_GENERATOR_API_ENDPOINT)
         except requests.exceptions.RequestException as e:
             raise CouldNotGetNameError(e)
 
-        current_time = int(time.time())
-        birthday = random.randint(
-            current_time - (SECONDS_IN_YEAR * MAX_YEARS_OLD),
-            current_time)
-        birthday_datetime = time.strftime('%Y-%m-%d %H:%M:%S',
-                                          time.localtime(birthday))
+        current_time = get_current_time()
+        birthday = get_birthday(current_time)
+        birthday_datetime = to_datetime(birthday)
 
         yield {"name": name,
                "birthday": birthday_datetime}
--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -1,9 +1,31 @@
-# import pytest
+import pytest
+import mock
+import time
+from catinabox import catgenerator
 
-# from catinabox import catgenerator
+
+@pytest.mark.parametrize('url, expected', [("http://namey.muffinlabs.com/name.json", "Jessica")])
+def test_get_name(url, expected):
+    return catgenerator.get_name(url) == expected
 
 
-# Write tests for the refactored `catinabox.catgenerator`
+def test_get_time(mock):
+    mock_time = mock.patch.object(time, "time", autospec=True)
+    mock_time.return_value = 7
+    assert catgenerator.get_current_time() == 7
 
-def test__():
-    pass
+
+@pytest.mark.parametrize('current_time, birthday', [(7, -102251259)])
+def test_get_birthday(mock, current_time, birthday):
+    mock_time = mock.patch.object(time, "time", autospec=True)
+    mock_time.return_value = current_time
+    assert catgenerator.get_birthday(current_time) == birthday
+
+
+@pytest.mark.parametrize('birthday, datetime', [(-102251259, '1966-10-05 08:52:21')])
+def test_to_datetime(birthday, datetime):
+    assert catgenerator.to_datetime(birthday) == datetime
+
+
+# def test_get_generator():
+#     pass
