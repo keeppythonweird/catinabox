42aafdfaba351632e4ceea8d58ba279bd358b0cf
JJ <jjisnow@gmail.com>
2019-10-09 22:03:28 +1100

    get_name is an object?

--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -19,7 +19,7 @@ def test_get_birthday(mocker):
 
 
 def test_cat_generator(mocker):
-    mock_name = mocker.patch(get_name, autospec=True)
+    mock_name = mocker.patch.object(get_name, autospec=True)
     mock_name.side_effect = ["David", "Moe"]
     mock_bday = mocker.patch('get_birthday')
     mock_bday.return_value = 'birthday'
