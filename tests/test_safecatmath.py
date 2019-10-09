3181854a22909b7e820f21c579db5491f903826d
JJ <jjisnow@gmail.com>
2019-10-09 14:54:12 +1100

    add safecatmath tests

--- tests/test_safecatmath.py
+++ tests/test_safecatmath.py
@@ -1,4 +1,5 @@
 # import pytest
+import pytest
 
 from catinabox import safecatmath
 
@@ -19,17 +20,24 @@ def test__cat_years_to_hooman_years__0__returns_0():
 
 
 def test__cat_years_to_hooman_years__less_0__raises():
-    assert True
+    with pytest.raises(safecatmath.InvalidAge):
+        cat_age = -1
+        assert safecatmath.cat_years_to_hooman_years(cat_age)
 
 
 def test__cat_years_to_hooman_years__older_than_1000__raises():
-    assert True
+    with pytest.raises(safecatmath.InvalidAge):
+        cat_age = 1001
+        assert safecatmath.cat_years_to_hooman_years(cat_age)
 
 
 def test__cat_years_to_hooman_years__string__raises():
-    assert True
+    with pytest.raises(safecatmath.InvalidAge):
+        cat_age = 'five'
+        assert safecatmath.cat_years_to_hooman_years(cat_age)
 
 
 def test__cat_years_to_hooman_years__nan__raises():
-    # hooman_age = float('nan')
-    assert True
+    with pytest.raises(safecatmath.InvalidAge):
+        cat_age = float('nan')
+        assert safecatmath.cat_years_to_hooman_years(cat_age)
