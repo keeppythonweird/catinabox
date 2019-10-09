a7ad9145350808ac80e22191bba662faab847da0
JJ <jjisnow@gmail.com>
2019-10-09 19:42:58 +1100

    simplify test_catactivities.py test

--- tests/test_catactivities.py
+++ tests/test_catactivities.py
@@ -1,19 +1,16 @@
-# import pytest
-import time
-
 import pytest
 
 from catinabox import catactivities
 
 
 def test__cat_nap__satisfying_nap(mocker):
-    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep = mocker.patch('time.sleep', autospec=True)
     catactivities.cat_nap(301)
     mock_sleep.assert_called_with(301)
 
 
 def test__cat_nap__not_satisfying(mocker):
-    mock_sleep = mocker.patch.object(time, "sleep", autospec=True)
+    mock_sleep = mocker.patch('time.sleep', autospec=True)
     with pytest.raises(catactivities.NapWillNotBeSatisfying):
         catactivities.cat_nap(299)
     assert mock_sleep.call_count == 0
