144e996a82d9366d418b6749dcd073b3c1540710
Leonid Lyamanov <llyamanov@linkedin.com>
2019-10-08 18:27:39 -0700

    add test coverage for cattery and catactivities

--- tests/test_cattery.py
+++ tests/test_cattery.py
@@ -7,33 +7,35 @@ from catinabox import cattery
 # add_cats
 ###########################################################################
 
-def test__add_cats__succeeds():
+@pytest.fixture()
+def cattery_client():
     c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Snookums"])
-    assert c.cats == ["Fluffy", "Snookums"]
-    assert c.num_cats == 2
+    return c
+
+
+def test__add_cats__succeeds(cattery_client):
+    cattery_client.add_cats(["Fluffy", "Snookums"])
+    assert cattery_client.cats == ["Fluffy", "Snookums"]
+    assert cattery_client.num_cats == 2
 
 
 ###########################################################################
 # remove_cat
 ###########################################################################
 
-def test__remove_cat__succeeds():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy", "Junior"])
-    c.remove_cat("Fluffy")
-    assert c.cats == ["Junior"]
-    assert c.num_cats == 1
+def test__remove_cat__succeeds(cattery_client):
+    cattery_client.add_cats(["Fluffy", "Junior"])
+    cattery_client.remove_cat("Fluffy")
+    assert cattery_client.cats == ["Junior"]
+    assert cattery_client.num_cats == 1
 
 
-def test__remove_cat__no_cats__fails():
-    c = cattery.Cattery()
+def test__remove_cat__no_cats__fails(cattery_client):
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Fluffles")
+        cattery_client.remove_cat("Fluffles")
 
 
-def test__remove_cat__cat_not_in_cattery__fails():
-    c = cattery.Cattery()
-    c.add_cats(["Fluffy"])
+def test__remove_cat__cat_not_in_cattery__fails(cattery_client):
+    cattery_client.add_cats(["Fluffy"])
     with pytest.raises(cattery.CatNotFound):
-        c.remove_cat("Snookums")
+        cattery_client.remove_cat("Snookums")
