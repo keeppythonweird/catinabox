f5623b6a162021c65e7021be7b48d7796d75509b
JJ <jjisnow@gmail.com>
2019-10-09 22:20:16 +1100

    simplify imports to get catgenerator to work

--- tests/test_catgenerator.py
+++ tests/test_catgenerator.py
@@ -1,12 +1,12 @@
-from catinabox.catgenerator import get_name
-from catinabox.catgenerator import get_birthday
+# from catinabox.catgenerator import get_name
+# from catinabox.catgenerator import get_birthday
 from catinabox import catgenerator
 
 
 def test_get_name(mocker):
     mock_get = mocker.patch('requests.get', autospec=True)
     mock_get.return_value.json.return_value = (['tim'],)
-    assert get_name() == ['tim']
+    assert catgenerator.get_name() == ['tim']
 
 
 def test_get_birthday(mocker):
@@ -14,13 +14,13 @@ def test_get_birthday(mocker):
     mock_time.side_effect = (catgenerator.SECONDS_IN_YEAR * 35,)
     mock_bday = mocker.patch('random.randint')
     mock_bday.return_value = catgenerator.SECONDS_IN_YEAR * 2
-    assert get_birthday() == "1972-01-01 00:00:00"
+    assert catgenerator.get_birthday() == "1972-01-01 00:00:00"
     mock_bday.assert_called_with(catgenerator.SECONDS_IN_YEAR * 5,
                                  catgenerator.SECONDS_IN_YEAR * 35)
 
 
 def test_cat_generator(mocker):
-    mock_name = mocker.patch.object(test_cat_generator.get_name, autospec=True)
+    mock_name = mocker.patch('get_name')
     mock_name.side_effect = ["David", "Moe"]
     mock_bday = mocker.patch('get_birthday')
     mock_bday.return_value = 'birthday'
