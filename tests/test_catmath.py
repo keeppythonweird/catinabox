a592a76fe0e6c7fdab9dfda6fe86ebce88f1a719
Dennis Akpenyi <dennisakpenyi@gmail.com>
2020-02-08 04:11:06 +0100

    pytest task updated.

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,15 +2,20 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert catmath.cat_years_to_hooman_years(11) == 55
+    cat_age = 11
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == 55
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert catmath.cat_years_to_hooman_years(0.1) <= 1.0
+    cat_age = 0.1
+    hooman_age = catmath.cat_years_to_hooman_years(cat_age)
+    assert hooman_age == 0.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert catmath.cat_years_to_hooman_years(0) == 0
+    hooman_age = catmath.cat_years_to_hooman_years(0)
+    assert hooman_age == 0
 
 
 # BONUS MATERIAL FOR STEP 2
