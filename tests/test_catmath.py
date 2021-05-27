95621638fdef59caecbc85c4cb31973813831e55
Anilitica <matheus.pinto@ug.uchile.cl>
2021-05-26 23:03:01 -0400

    Parametrizing input values, invalid input and fixture

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -1,6 +1,16 @@
 from catinabox import catmath
+import pytest
 
 
+@pytest.mark.parametrize("age, expected", [
+    (10, 50),
+    (0.5, 2.5),
+    (0, 0)
+])
+def test_cat_to_hooman(age, expected):
+    assert catmath.cat_years_to_hooman_years(age) == expected
+
+"""
 def test__cat_years_to_hooman_years__middle_age__succeeds():
     assert catmath.cat_years_to_hooman_years(10) == 50
 
@@ -11,6 +21,7 @@ def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
 
 def test__cat_years_to_hooman_years__0__returns_0():
     assert catmath.cat_years_to_hooman_years(0) == 0
+"""
 
 
 # BONUS MATERIAL FOR STEP 2
--- tests/test_safecatmath.py
+++ tests/test_safecatmath.py
@@ -3,6 +3,15 @@ import pytest
 from catinabox import safecatmath
 
 
+@pytest.mark.parametrize("age, expected", [
+    (7, 35),
+    (0.1, 0.5),
+    (0, 0)
+])
+def test__cat_years_to_hooman_years(age, expected):
+    assert safecatmath.cat_years_to_hooman_years(age) == expected
+
+"""
 def test__cat_years_to_hooman_years__middle_age__succeeds():
     hooman_age = safecatmath.cat_years_to_hooman_years(7)
     assert hooman_age == 35
@@ -16,8 +25,20 @@ def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
 def test__cat_years_to_hooman_years__0__returns_0():
     hooman_age = safecatmath.cat_years_to_hooman_years(0)
     assert hooman_age == 0
+"""
 
 
+@pytest.mark.parametrize("age", [
+    -5,
+    2100,
+    "two",
+    float('nan')
+])
+def test__cat_years_to_hooman_years__invalid_input__raises(age):
+    with pytest.raises(safecatmath.InvalidAge):
+        safecatmath.cat_years_to_hooman_years(age)
+
+"""
 def test__cat_years_to_hooman_years__less_0__raises():
     with pytest.raises(safecatmath.InvalidAge):
         safecatmath.cat_years_to_hooman_years(-5)
@@ -37,3 +58,4 @@ def test__cat_years_to_hooman_years__nan__raises():
     hooman_age = float('nan')
     with pytest.raises(safecatmath.InvalidAge):
         safecatmath.cat_years_to_hooman_years(hooman_age)
+"""
